56016bbc6cceaab34c891b5f07ef11022aca3630
Sai Sudhiksha Malla <sudhikshamalla28@gmail.com>
2021-12-03 17:09:11 +0530

    Added words in Family and Colours activities

--- app/src/main/java/com/example/android/miwoklearning/ColorsActivity.java
+++ app/src/main/java/com/example/android/miwoklearning/ColorsActivity.java
@@ -2,6 +2,9 @@ package com.example.android.miwoklearning;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class ColorsActivity extends AppCompatActivity {
 
@@ -9,5 +12,17 @@ public class ColorsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_colors);
+        ArrayList<Word> arr = new ArrayList<>();
+        arr.add(new Word("red","weṭeṭṭi",R.drawable.color_red));
+        arr.add(new Word("green","chokokki",R.drawable.color_green));
+        arr.add(new Word("brown","ṭakaakki",R.drawable.color_brown));
+        arr.add(new Word("gray","ṭopoppi",R.drawable.color_gray));
+        arr.add(new Word("black","kululli",R.drawable.color_black));
+        arr.add(new Word("white","kelelli",R.drawable.color_white));
+        arr.add(new Word("dusty yellow","ṭopiisә",R.drawable.color_dusty_yellow));
+        arr.add(new Word("mustard yellow","chiwiiṭә",R.drawable.color_mustard_yellow));
+        WordAdapter itemsAdapter = new WordAdapter(this,arr);
+        ListView listView = (ListView) findViewById(R.id.color_root_view);
+        listView.setAdapter(itemsAdapter);
     }
 }
--- app/src/main/java/com/example/android/miwoklearning/FamilyActivity.java
+++ app/src/main/java/com/example/android/miwoklearning/FamilyActivity.java
@@ -2,6 +2,9 @@ package com.example.android.miwoklearning;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class FamilyActivity extends AppCompatActivity {
 
@@ -9,5 +12,19 @@ public class FamilyActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_family);
+        ArrayList<Word> arr = new ArrayList<>();
+        arr.add(new Word("father","әpә",R.drawable.family_father));
+        arr.add(new Word("mother","әṭa",R.drawable.family_mother));
+        arr.add(new Word("son","angsi",R.drawable.family_son));
+        arr.add(new Word("daughter","tune",R.drawable.family_daughter));
+        arr.add(new Word("older brother","taachi",R.drawable.family_older_brother));
+        arr.add(new Word("younger brother", "chalitti",R.drawable.family_younger_brother));
+        arr.add(new Word("older sister", "teṭe",R.drawable.family_older_sister));
+        arr.add(new Word("younger sister", "kolliti",R.drawable.family_younger_sister));
+        arr.add(new Word("grandmother", "ama", R.drawable.family_grandmother));
+        arr.add(new Word("grandfather","paapa",R.drawable.family_grandfather));
+        WordAdapter itemsAdapter = new WordAdapter(this,arr);
+        ListView listView = (ListView) findViewById(R.id.family_root_view);
+        listView.setAdapter(itemsAdapter);
     }
 }
